dfd6582c277c27d10e7da9b82725a164a443df65
Crowley <handhfa45@gmail.com>
2024-02-05 23:42:26 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/donuts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.donuts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,110 @@
+import 'package:donuts/tab/burger_tab.dart';
+import 'package:donuts/tab/donut_tab.dart';
+import 'package:donuts/tab/pancake_tab.dart';
+import 'package:donuts/tab/pizza_tab.dart';
+import 'package:donuts/tab/smoothie_tab.dart';
+import 'package:donuts/util/my_tab.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Widget> myTabs = const [
+    MyTab(
+      iconPath: 'lib/icons/donut.png',
+    ),
+    MyTab(
+      iconPath: 'lib/icons/burger.png',
+    ),
+    MyTab(
+      iconPath: 'lib/icons/smoothie.png',
+    ),
+    MyTab(
+      iconPath: 'lib/icons/pancakes.png',
+    ),
+    MyTab(
+      iconPath: 'lib/icons/pizza.png',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.grey[800],
+                size: 36,
+              ),
+              onPressed: () {
+                //
+              },
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: IconButton(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.grey[800],
+                  size: 36,
+                ),
+                onPressed: () {
+                  //
+                },
+              ),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 36.0, vertical: 18),
+              child: Row(
+                children: [
+                  Text(
+                    'I want to',
+                    style: TextStyle(fontSize: 24, color: Colors.grey[600]),
+                  ),
+                  const Text(
+                    ' EAT',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+
+            TabBar(tabs: myTabs),
+
+            //
+            Expanded(
+              child: TabBarView(
+                children: [
+                  DonutTab(),
+                  BurgerTab(),
+                  SmoothieTab(),
+                  PancakeTab(),
+                  PizzaTab(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
